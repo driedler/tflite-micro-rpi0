cf0638bcb887b7427802daf9dbca6cbca15532f9
Dan Riedler <driedler@gmail.com>
2021-06-23 20:43:08 -0700

    build with -Ofast flag

--- tensorflow/lite/tools/cmake/rpi0_toolchain.cmake
+++ tensorflow/lite/tools/cmake/rpi0_toolchain.cmake
@@ -5,7 +5,7 @@ set(CMAKE_SYSTEM_PROCESSOR armv6)
 set(ARMCC_ROOT ${CMAKE_CURRENT_LIST_DIR}/toolchains/arm-rpi-linux-gnueabihf/x64-gcc-6.5.0/arm-rpi-linux-gnueabihf)
 set(ARMCC_PREFIX ${ARMCC_ROOT}/bin/arm-rpi-linux-gnueabihf-)
 
-set(ARMCC_FLAGS "-march=armv6 -mfpu=vfp -funsafe-math-optimizations")
+set(ARMCC_FLAGS "-march=armv6 -mfpu=vfp -funsafe-math-optimizations -Ofast")
 set(ARMCC_FLAGS "${ARMCC_FLAGS} -isystem ${ARMCC_ROOT}/lib/gcc/arm-rpi-gnueabihf/6.5.0/include")
 set(ARMCC_FLAGS "${ARMCC_FLAGS} -isystem ${ARMCC_ROOT}/lib/gcc/arm-rpi-gnueabihf/6.5.0/include-fixed")
 set(ARMCC_FLAGS "${ARMCC_FLAGS} -isystem ${ARMCC_ROOT}/arm-rpi-linux-gnueabihf/include/c++/6.5.0")
